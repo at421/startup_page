bb54a3f74082fe63491546be0320d6775b18e2f0
SwallowAI <alex@swallow.ai>
2025-05-11 17:36:23 +0000

    feat: migrate pages to app structure

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,11 @@
-import { Footer } from "@/components/footer/Footer";
 import { SignIn } from "@/components/signin/SignIn";
 import { Barlow } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Sign in to your account",
+};
 
 const barlowFont = Barlow({
   subsets: ["latin"],
@@ -8,7 +13,7 @@ const barlowFont = Barlow({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export default function signIn() {
+export default function Page() {
   return (
     <main className={barlowFont.className}>
       <SignIn />
